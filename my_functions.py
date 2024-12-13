5d6870e3322ea03283a5c20dd2b04abf8247585b
j4-metrics <68926735+j4-metrics@users.noreply.github.com>
2024-12-13 18:32:33 -0300

    updated my_functions, and built some correlation between variables

--- my_functions.py
+++ my_functions.py
@@ -2,10 +2,12 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def plot_scatter(df, x, y):
+def plot_scatter(data, x, y, hue):
     plt.figure(figsize=(8, 6))
-    sns.scatterplot(data=df, x=x, y=y)
+    sns.scatterplot(data=data, x=x, y=y, hue=hue, palette='tab10')
     plt.title(f'Relação entre {x} e {y}')
-    plt.xlabel(f'{x}')
-    plt.ylabel(f'{y}')
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.legend(title=hue, bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.tight_layout()  # Adjust layout to fit legend
     plt.show()
