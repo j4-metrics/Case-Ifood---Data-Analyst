81c77d7b49805dff0e3af4b675e6d59ade4b1ab7
j4-metrics <68926735+j4-metrics@users.noreply.github.com>
2024-12-15 13:20:11 -0300

    created function to plot boxplot and compare with table of income

--- my_functions.py
+++ my_functions.py
@@ -38,3 +38,58 @@ def check_outliers(data):
     df_describe['CheckBotOut'] = df_describe.apply(lambda row: 'Yes' if row['min'] < row["BotOutlier"] else 'No', axis=1) # mark for possible Bottom outliers
     df_describe['CheckTopOut'] = df_describe.apply(lambda row: 'Yes' if row['max'] > row["TopOutlier"] else 'No', axis=1) # mark for possible Top outliers
     return df_describe
+
+### Function to easily compare boxplot of restauantes_ativos by region and tabel of renda_media
+def boxplot_table(df, cluster):
+
+    # Define the figure and GridSpec layout
+    fig = plt.figure(figsize=(14, 8))
+    gs = GridSpec(2, 2, width_ratios=[3, 1], height_ratios=[3, 1], wspace=0.4, hspace=0.4)
+
+    # Boxplot in the lower-left section
+    ax_boxplot = fig.add_subplot(gs[:, 0])  # Occupies both rows in the first column
+    category_order = df[df['cluster'] == cluster].groupby('region')['restaurantes_ativos'].median().sort_values().index
+    sns.boxplot(
+        x='region',
+        y='restaurantes_ativos',
+        data=df[df['cluster'] == cluster],
+        order=category_order,
+        ax=ax_boxplot
+    )
+    ax_boxplot.set_title('restaurantes_ativos distribution per region level')
+    ax_boxplot.set_xlabel('')
+    ax_boxplot.set_ylabel('Restaurantes_ativos (%)')
+
+    # Add median values as text annotations
+    medians = df[df['cluster'] == cluster].groupby('region')['restaurantes_ativos'].median()
+    for i, region in enumerate(category_order):
+        median = medians[region]
+        ax_boxplot.text(
+            i, median, f'{median:.1f}',
+            ha='center', va='bottom', color='black', fontsize=10, weight='bold'
+        )
+
+    # Aggregated table in the upper-right section
+    ax_table = fig.add_subplot(gs[0, 1])  # Upper-right quarter
+    aggregation = df[df['cluster'] == cluster].groupby('region').agg({
+        'cidade': 'count',
+        'renda_media': 'median'
+    }).rename(columns={'cidade':'cidades', 'renda_media':'renda (R$)'})
+    aggregation = aggregation.reindex(category_order).astype({'cidades': 'int', 'renda (R$)': 'int'})
+
+
+    # Hide the axes for the table and add the data
+    ax_table.axis('tight')
+    ax_table.axis('off')
+    table = ax_table.table(
+        cellText=aggregation.values,
+        colLabels=aggregation.columns,
+        rowLabels=aggregation.index,
+        cellLoc='center',
+        loc='center'
+    )
+    table.auto_set_font_size(False)
+    table.set_fontsize(10)
+    table.auto_set_column_width(col=list(range(len(aggregation.columns) + 1)))
+
+    plt.show()
